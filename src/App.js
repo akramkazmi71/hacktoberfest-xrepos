fe823e02913993c0be7b8428b523b0177faeef05
Md Akram Kazmi <akramkazmi71@gmail.com>
2019-10-05 19:17:28 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -5,67 +5,86 @@ class App extends Component{
   constructor() {
     super();
     this.state = {
-      list: [],
       size: 0,
       pageNumber: 1,
-      max_page: 1,
-      count: 0
+      urlList: []
+
     };
   }
 
   componentDidMount() {
-    this.callPage();
+    this.callPage(1);
   }
 
-  callPage = () => {
-    fetch(`https://api.github.com/search/issues?page=${this.state.pageNumber}&q=author:hacktoberfest-team`)
+  callPage = (pageNo) => {
+    //Fetching data from the API.
+    fetch(`https://api.github.com/search/issues?page=${pageNo}&q=author:hacktoberfest-team`)
       .then(response => {
         return response.json();
       })
       .then(response => {
         let tempArr=[]  
+        //Storing the URLs from the API into tempArr[].
         for (var i = 0; i < response.items.length; i++) {
           tempArr.push(response.items[i].url+"\n")
         }
+        //Storing total number of entries into totalCount.
+        var totalCount=response.items.length
+        //Calling stringSlice function to remove junk values from urls.
+        this.stringSlice(tempArr,totalCount)
 
-        this.setState({
-          list: tempArr,
-          size: response.total_count
-        })
       });
   }
     
   nextPage = () => {
     var x=this.state.size/30
-    this.state.max_page=Math.floor(x)+1
-    this.state.pageNumber=this.state.pageNumber+1
-    if(this.state.pageNumber<=this.state.max_page){
-      this.callPage();
+    //Finding maximum number of pages that are formed.
+    var maxPage=Math.floor(x)+1
+    if(this.state.pageNumber<=maxPage){
+      var incrementPage=this.state.pageNumber+1
+      //Going to next page.
+      this.callPage(incrementPage);
+      this.setState({
+        pageNumber: incrementPage
+      })
     }
   }
 
   previousPage = () => {
-    this.state.pageNumber-=1
-    this.state.count-=(this.state.count%30)+30
-    this.callPage();
+    var prevPage=this.state.pageNumber-1
+    //Going to previous page.
+    this.callPage(prevPage);
+    this.setState({
+      pageNumber: prevPage
+    })
   }
 
-  render() {
+  stringSlice = (tempArr,totalCount) => {
     const urls=[]
-    for (const [index, value] of this.state.list.entries()){
+    for (const [index, value] of tempArr.entries()){
+      //Converting undefined data into string.
       var url0=JSON.stringify({value})
       var url1=String(url0)
+      //Removing junk values from url.
       var url2=url1.replace('{"value":"','')
       var url3=url2.replace('n"}','')
       var url4=url3.replace('api.','www.')
       var final_url=url4.replace('/repos','')
-      this.state.count++
+      //Storing valid url into urls[].
       urls.push(<p key={index}><a href={final_url}>{index+1}.{final_url}</a></p>)
       urls.sort()
     }
+    this.setState({
+      urlList: urls,
+      size: totalCount
+    })
+  }
+
+  render() {
+    
     return(
       <div>
-        {urls}  
+        {this.state.urlList}  
         {this.state.pageNumber>1 && <button onClick={this.previousPage}>Previous Page</button>}
         <button onClick={this.nextPage}>Next Page</button> 
       </div>
