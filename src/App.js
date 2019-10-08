b9d018ea0c0fc21ed4c9031d39b24a2cca81885d
Cristiano <cristiano.goncalves@cubos.io>
2019-10-07 23:56:57 -0300

    feature: improve UI design and refact some css.

--- src/App.js
+++ src/App.js
@@ -1,113 +1,139 @@
-import React, {Component} from 'react';
-import './App.css';
-import Loader from './loader/loader'
+import React, { Component } from "react";
+import "./App.css";
+import Loader from "./components/Loader";
+import Card from "./components/Card";
 
-class App extends Component{
-  constructor() {
-    super();
-    this.state = {
-      size: 0,
-      pageNumber: 1,
-      urlList: [],
-      loader:false,
-      repoName: [],
-      repoUrl: [],
-      total_count: 0
-    };
-  }
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			size: 0,
+			pageNumber: 1,
+			urlList: [],
+			loader: false,
+			repoName: [],
+			repoUrl: [],
+			total_count: 0
+		};
+	}
 
-  componentDidMount() {
-    this.callPage(1,30);
-  }
+	componentDidMount() {
+		this.callPage(1, 30);
+	}
 
-  callPage = (pageNo,perPage) => {
-    //Fetching data from the API.
-    this.setState({
-      ...this.state,
-      loader:true
-    })
-    fetch(`https://api.github.com/search/issues?page=${pageNo}&per_page=${perPage}&q=author:hacktoberfest-team`)
-      .then(response => {
-        return response.json();
-      })
-      .then(response => {
-        let total_count = response['total_count'];
-        let tempArr=[]
-        //Storing the URLs from the API into tempArr[].
-        for (var i = 0; i < response.items.length; i++) {
-          tempArr.push(response.items[i].html_url+"\n")
-        }
-        //Storing total number of entries into totalCount.
-        var totalCount=response.items.length
-        //Calling stringSlice function to remove junk values from urls.
-        this.stringSlice(tempArr,totalCount)
-        this.setState({
-          ...this.state,
-          loader:false
-        })
-        if(this.state.total_count !== total_count){
-          this.setState({
-            total_count: total_count
-          });
-        }
-      });
-  }
+	callPage = (pageNo, perPage) => {
+		//Fetching data from the API.
+		this.setState({
+			...this.state,
+			loader: true
+		});
+		fetch(
+			`https://api.github.com/search/issues?page=${pageNo}&per_page=${perPage}&q=author:hacktoberfest-team`
+		)
+			.then(response => {
+				return response.json();
+			})
+			.then(response => {
+				let total_count = response["total_count"];
+				let tempArr = [];
+				//Storing the URLs from the API into tempArr[].
+				for (var i = 0; i < response.items.length; i++) {
+					tempArr.push(response.items[i].html_url + "\n");
+				}
+				//Storing total number of entries into totalCount.
+				var totalCount = response.items.length;
+				//Calling stringSlice function to remove junk values from urls.
+				this.stringSlice(tempArr, totalCount);
+				this.setState({
+					...this.state,
+					loader: false
+				});
+				if (this.state.total_count !== total_count) {
+					this.setState({
+						total_count: total_count
+					});
+				}
+			});
+	};
 
+	loadMore = () => {
+		var incrementPage = this.state.pageNumber + 1;
+		let perPage = 30;
+		this.callPage(incrementPage, perPage);
+		this.setState({
+			pageNumber: incrementPage
+		});
+	};
 
-  loadMore = () => {
-    var incrementPage=this.state.pageNumber+1
-    let perPage = 30;
-    this.callPage(incrementPage, perPage);
-    this.setState({
-      pageNumber: incrementPage
-    });
-  }
+	stringSlice = (tempArr, totalCount) => {
+		const urls = [];
+		const repo = [];
+		let { urlList, repoName, size } = this.state;
+		for (const [index, value] of tempArr.entries()) {
+			urls.push(value);
+			let urlSplit = value.replace(
+				/https:\/\/github.com\/|\/issues\/[0-9]+/g,
+				""
+			);
+			var repName = urlSplit.split("/");
+			var repAddress = "https://github.com/" + urlSplit;
+			repo.push(
+				<Card
+					repName={repName[1]}
+					index={size + index + 1}
+					key={size + index}
+					repoIssue={value}
+					repAddress={repAddress}
+				/>
+			);
+		}
+		urlList = urlList.concat(urls);
+		repoName = repoName.concat(repo);
+		this.setState({
+			urlList: urlList,
+			size: size + totalCount,
+			repoName: repoName
+		});
+	};
 
-  stringSlice = (tempArr,totalCount) => {
-    const urls=[]
-    const repo=[]
-    let {urlList, repoName, size} = this.state;
-    for (const [index, value] of tempArr.entries()){
-      urls.push(value)
-      let urlSplit=value.replace(/https:\/\/github.com\/|\/issues\/[0-9]+/g,'')
-      var repName=urlSplit.split("/")
-      var repAddress="https://github.com/"+urlSplit
-      repo.push(<div class="card"><p key={size+index}>{size+index+1}.{repName[1]}</p>
-                  <button class="repoIssue"><a href={value}>Issue</a></button>
-                  <button class="repoIssue"><a href={repAddress}>Repository</a></button>
-                </div>)
-    }
-    urlList = urlList.concat(urls);
-    repoName = repoName.concat(repo);
-    this.setState({
-      urlList: urlList,
-      size: size+totalCount,
-      repoName: repoName
-    })
-  }
+	hasMore = () => {
+		return this.state.size < this.state.total_count;
+	};
 
-  hasMore = () => {
-    return this.state.size < this.state.total_count;
-  }
+	render() {
+		return (
+			<>
+				<a
+					href="https://github.com/akramkazmi71/hacktoberfest-xrepos"
+					className="forkLink"
+				>
+					<img
+						border="0"
+						alt="Github"
+						src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+						width="150"
+						height="150"
+					></img>
+				</a>
+				<h2>Hacktoberfest Excluded Repositories</h2>
+				<div className="container">
+					{this.state.loader ? (
+						<Loader loader={this.state.loader} />
+					) : (
+						this.state.repoName
+					)}
+				</div>
 
-  render() {
-    return(
-      <div class="background">
-        <a href="https://github.com/akramkazmi71/hacktoberfest-xrepos">
-        <img border="0" alt="Github" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" width="150" height="150"></img></a>
-        <h2><u>Hacktoberfest Excluded Repositories</u></h2>
-        <div class="container">
-          {this.state.loader ?
-            <Loader loader={this.state.loader}/>
-            :
-            this.state.repoName
-          }
-         {this.hasMore() && <button class="nextPrev" onClick={this.loadMore}>Load More</button>}
-        </div>
-      </div>
-    )
-  }
+				<footer className="footer">
+					{this.state.loader && this.hasMore() && (
+						<button className="nextPrev" onClick={this.loadMore}>
+							Load More
+						</button>
+					)}
+				</footer>
+			</>
+		);
+	}
 }
 
 export default App;
-
